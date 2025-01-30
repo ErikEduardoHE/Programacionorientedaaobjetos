fbc47d016486f004b8bb03b331398b1553fdd897
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-01-30 15:57:51 -0600

    Upload trabajo1.py

--- trabajo1.py
+++ trabajo1.py
@@ -5,20 +5,20 @@ import math
 
 
 while True:
-    nL = input("Ingresa el numero de lados de la figura(3,4,5): ")
+    nL = int(input("Ingresa el numero de lados de la figura(3,4,5): "))
     
-    if nL == "3":
-       L = input("Ingresa la medida de los lados de cada figura: ")
+    if nL == 3:
+       L = int(input("Ingresa la medida de los lados de cada figura: "))
        perimetro(L,nL)
        arearectangulo(L)
 
-    elif nL == "4":
-        L = input("Ingresa la medida de los lados de cada figura: ")
+    elif nL == 4:
+        L = int(input("Ingresa la medida de los lados de cada figura: "))
         perimetro(L,nL)
         areacuadrado(L)
 
-    elif nL == "5":
-        L = input("Ingresa la medida de los lados de cada figura: ")
+    elif nL == 5:
+        L = int(input("Ingresa la medida de los lados de cada figura: "))
         perimetro(L,nL)
         areapentagono(L)
         
@@ -29,14 +29,17 @@ while True:
     
     def areacuadrado(L):
         return L * L
+        print(areacuadrado(L))
             
     def arearectangulo(L):
         h =  3**2 * L / 2
         return L * h / 2
+        print(arearectangulo(L))
         
     def areapentagono(L):
         return nL / 2 (math.tan( ap ))
+        print(areapentagono(L))
 
     def perimetro(L,nL):
         return L * nL
-        
+        print(perimetro(L,nL))
