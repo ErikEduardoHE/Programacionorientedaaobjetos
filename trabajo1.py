9ee7a2090e9fc6f595c99cdd6145cbb238c11e72
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-01-31 09:47:58 -0600

    Upload trabajo1.py

--- trabajo1.py
+++ trabajo1.py
@@ -23,24 +23,38 @@ while True:
     if nL == 3:
        L = int(input("Ingresa la medida de los lados de cada figura: "))
        perimetro(L,nL)
+       print("-------------------------")
        print(f"Perimetro: {perimetro(L,nL)}")
+       print("-------------------------")
        areatriangulo(L)
+       print("-------------------------")
        print(f"Area: {areatriangulo(L)}")
+       print("-------------------------")
 
     elif nL == 4:
         L = int(input("Ingresa la medida de los lados de cada figura: "))
         perimetro(L,nL)
+        print("-------------------------")
         print(f"Perimetro: {perimetro(L,nL)}")
+        print("-------------------------")
         areacuadrado(L)
+        print("-------------------------")
         print(f"Area: {areacuadrado(L)}")
+        print("-------------------------")
 
     elif nL == 5:
         L = int(input("Ingresa la medida de los lados de cada figura: "))
         
         perimetro(L,nL)
+        print("-------------------------")
         print(f"Perimetro: {perimetro(L,nL)}")
+        print("-------------------------")
         areapentagono(L)
+        print("-------------------------")
         print(f"Area: {areapentagono(L)}")
+        print("-------------------------")
         
     else:
+        print("-------------------------")
         print("Error")
+        print("-------------------------")
