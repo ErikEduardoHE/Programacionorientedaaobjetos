42cb3e92408eb9e6afe62e2ac7da43bb97c18ae8
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-02-05 10:46:33 -0600

    Upload trabajo1.py

--- trabajo1.py
+++ trabajo1.py
@@ -1,39 +1,43 @@
 import math
-class figura:
-    def cuadrado(L):
+
+class Figura:
+    
+    def cuadrado(self, L):
         return L * L
                     
-    def triangulo(L):
+    def triangulo(self, L):
         altura = (math.sqrt(3) / 2) * L
         return (L * altura) / 2
             
-    def figurasregulares(L):
+    def figurasregulares(self, L, nL):
         angulo_radianes = (math.pi / nL)
-        return nL * L**2 / 4 * math.tan(angulo_radianes)
+        return (nL * L**2) / (4 * math.tan(angulo_radianes))
 
-    def perimetro(L, nL):
+    def perimetro(self, L, nL):
         return L * nL
+
+figura = Figura()
 
 while True:
     nL = int(input("Ingresa el numero de lados de la figura: "))
     
     if nL == 1:
-        print("No puedo calcular esa figura aun")
+        print("No puedo calcular esa figura aún.")
    
     elif nL == 2:
-        print("Figura no existente")
+        print("Figura no existente.")
    
     elif nL >= 3:
-        L = float(input("Ingresa la medida de los lados de cada figura: "))
-        perim = perimetro(L, nL)
+        L = float(input("Ingresa la medida de los lados de la figura: "))
+        perim = figura.perimetro(L, nL) 
         print("-------------------------")
-        print(f"Perimetro: {perim}")
+        print(f"Perímetro: {perim}")
         print("-------------------------")
-        area = figurasregulares(L)
-        print(f"Area: {area:.2f}")
+        area = figura.figurasregulares(L, nL) 
+        print(f"Área: {area:.2f}")
         print("-------------------------")
         
     else:
         print("-------------------------")
-        print("Aun no puedo calcular esa figura")
+        print("Aún no puedo calcular esa figura.")
         print("-------------------------")
