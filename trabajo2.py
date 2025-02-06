0d081b9cf03b9b1284e4870138f759b87b815be1
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-02-06 08:18:55 -0600

    Update trabajo2.py

--- trabajo2.py
+++ trabajo2.py
@@ -20,3 +20,23 @@ def eliminarelementoporindice(lista):
     print(f"{} eliminado de la lista")
     del lista[0]
 
+while True:
+    opcion = input("Elige una opcion: ")
+    print("1.- Agregar un objeto")
+    print("2.- Remover elemento")
+    print("3.- Eliminar ultimo elemento")
+    print("4.- Eliminar elemento por indice")
+    print("5.- Salir")
+
+    if opcion == "1":
+        pass
+    elif opcion == "2":
+        pass
+    elif opcion == "3":
+        pass
+    elif opcion == "4":
+        pass
+    elif opcion == "5":
+        break
+    else:
+        print("Elige una opcion valida")
