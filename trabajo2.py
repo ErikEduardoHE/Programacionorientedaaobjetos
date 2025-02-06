cb483ddd1337b4b4794de849e9e9cc8da7d11998
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-02-06 08:37:38 -0600

    Update trabajo2.py

--- trabajo2.py
+++ trabajo2.py
@@ -8,18 +8,19 @@ def insertarenlista(lista):
     print(f"{objingresado} agregado a la lista")
 
 def removerdelista(lista):
-    lista.remove()
-    print(f"{lista} removido de la lista")
+    lista.remove(objremovido)
+    print(f"{objremovido} removido de la lista")
 
 def eliminarultimoelemento(lista):
     lista.pop()
-    print(f"Se elimino el ultimo elemento: {lista}")
+    print(f"Se elimino el ultimo elemento")
 
 def eliminarelementoporindice(lista):
-    print(f"{lista} eliminado de la lista")
-    del lista[0]
+    print(f"{lista[indice]} eliminado de la lista")
+    del lista[indice]
 
 while True:
+    print("-----------------------------------")
     print("Elige una opcion: ")
     print("1.- Agregar un objeto")
     print("2.- Remover elemento")
@@ -27,18 +28,33 @@ while True:
     print("4.- Eliminar elemento por indice")
     print("5.- Imprimir lista")
     print("6.- Salir")
+    print("-----------------------------------")
     opcion = input(">>> ")
     if opcion == "1":
+        print("-----------------------------------")
         objingresado = input("Que elemento deseas ingresar: ")
         insertarenlista(lista)
+        print("-----------------------------------")
     elif opcion == "2":
-        pass
+        print("-----------------------------------")
+        objremovido = input("Que elemento quieres remover: ")
+        removerdelista(lista)
+        print("-----------------------------------")
     elif opcion == "3":
-        pass
+        print("-----------------------------------")
+        eliminarultimoelemento(lista)
+        print("-----------------------------------")
     elif opcion == "4":
-        pass
-    elif opcion == "5":
+        print("-----------------------------------")
         print(lista)
+        indice = int(input("Escribe el indice del objeto que quieres eliminar: "))
+        indice = indice - 1
+        eliminarelementoporindice(lista)
+        print("-----------------------------------")
+    elif opcion == "5":
+        print("-----------------------------------")
+        print(lista)
+        print("-----------------------------------")
     elif opcion == "6":
         break
     else:
