07ee139fd97a62737afa89365744163f1a4ab75c
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-02-06 09:40:34 -0600

    Upload Ejerciciolista.py

--- Ejerciciolista.py
+++ Ejerciciolista.py
@@ -3,6 +3,20 @@ class ListaManager:
         self.lista = [] 
 
     def insertar_en_lista(self, objingresado):
+        print("Que tipo de elemento es: ")
+        print("1.- Numero enetero")
+        print("2.- Numero decimal")
+        print("3.- Cadena de texto")
+        tipovalor = input(">>>>>> ")
+        if tipovalor == "1":
+            objingresado = int(objingresado)
+        elif tipovalor == "2":
+            objingresado = float(objingresado)
+        elif tipovalor == "3":
+            objingresado = str(objingresado)
+        else:
+            print("Elige un tipo de valor valido")
+        
         self.lista.append(objingresado)
         print(f"{objingresado} agregado a la lista")
 
@@ -31,6 +45,21 @@ class ListaManager:
     def imprimir_lista(self):
         print(f"Lista actual: {self.lista}")
 
+    def modificar_tipo_elemento(self, tipodato):
+        print("A que tipo de elemento quieres convertirlo: ")
+        print("1.- Numero enetero")
+        print("2.- Numero decimal")
+        print("3.- Cadena de texto")
+        tipovalor = input(">>>>>> ")
+        if tipovalor == "1":
+            tipodato = int(tipodato)
+        elif tipovalor == "2":
+            tipodato = float(tipodato)
+        elif tipovalor == "3":
+            tipodato = str(tipodato)
+        else:
+            print("Elige un tipo de valor valido")
+
 lista_manager = ListaManager()
 
 while True:
@@ -40,8 +69,9 @@ while True:
     print("2.- Remover elemento")
     print("3.- Modificar elemento")
     print("4.- Eliminar elemento por índice")
-    print("5.- Imprimir lista")
-    print("6.- Salir")
+    print("5.- Modificar tipo de dato")
+    print("6.- Imprimir lista")
+    print("7.- Salir")
     print("-----------------------------------")
     opcion = input(">>>>>> ")
 
@@ -80,13 +110,16 @@ while True:
         except ValueError:
             print("Por favor ingresa un número válido.")
         print("-----------------------------------")
-
     elif opcion == "5":
+        tipodato = int(input("Escribe el indice del elemento a modificar: "))
+        tipodato = tipodato - 1
+
+    elif opcion == "6":
         print("-----------------------------------")
         lista_manager.imprimir_lista()
         print("-----------------------------------")
 
-    elif opcion == "6":
+    elif opcion == "7":
         break
 
     else:
