dad591b6f2b69728ceb8d34910ec8e8c9fd37ef6
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-02-06 08:13:43 -0600

    Update trabajo2.py

--- trabajo2.py
+++ trabajo2.py
@@ -1,2 +1,22 @@
+# Hacer un areglo que ingrese, elimine, modifique y detecte elementos en una lista
 from numpy import array
 import numpy as np
+
+lista = []
+
+def insertarenlista(lista):
+    lista.append()
+    print(f"{lista} agregado a la lista")
+
+def removerdelista(lista):
+    lista.remove()
+    print(f"{lista} removido de la lista")
+
+def eliminarultimoelemento(lista):
+    lista.pop()
+    print(f"Se elimino el ultimo elemento: {lista}")
+
+def eliminarelementoporindice(lista):
+    print(f"{} eliminado de la lista")
+    del lista[0]
+
