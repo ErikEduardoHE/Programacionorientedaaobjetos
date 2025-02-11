825c10a499997f3a4ddc48c87b14c9a1614352ba
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-02-11 09:34:37 -0600

    Update PilaresPOO.py

--- PilaresdePOO.py
+++ PilaresdePOO.py
@@ -23,7 +23,7 @@ class Rectangulo(Figura):
     def __init__(self, ancho, alto):
         self.ancho = ancho
         self.alto = alto
-
+             
     def calcular_area(self):
         return self.ancho * self.alto
 
