9183241017595e79bea6b0540f5fbd373561eac9
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-03-27 08:30:38 -0600

    Update colecciones.py

--- Colecciones.py
+++ Colecciones.py
@@ -19,8 +19,19 @@ tupla = tuple()
 import numpy as np
 from numpy import array
 
-lista_mixta = ["Hola mundo", 1.01, 1, True, False]
+lista_mixta = ["Hola mundo", 3.1416 , 1, True, False]
 
-lista_mixta_numpy = np.array([1, 2, 3, 4, 5])
+lista_numpy = np.array([1, 2, 3, 4, 5])
 
+tupla_mixta = tuple(lista_mixta)
 
+tupla_numpy = tuple(lista_numpy)
+
+conjunto_mixto = set(lista_mixta)
+
+conjunto_numpy = set(lista_numpy)
+
+diccionario = {
+    "Nombre": ,
+    "Edad": 10
+}
