6ffded93bd10b41639a0a3f308344c8b360336ae
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-03-27 07:59:11 -0600

    Upload Areayperimetro.py

---
+++ Colecciones.py
@@ -0,0 +1,18 @@
+""" Colecciones
+
+Listas 
+Tuplas
+Conjuntos
+Diccionarios
+
+import numpy as np
+from numpy import array
+np.array()
+arreglo = []
+arreglo = list()
+
+String > Floats > int > boolean (gerarquia ed mayor peso en memotia)
+
+tupla = (,) (una tupla es inmutable)
+tupla = tuple()
+"""
