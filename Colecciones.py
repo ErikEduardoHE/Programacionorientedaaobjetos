1eade655dc6bcaf84070652ea46952b9717708a1
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-03-27 08:21:54 -0600

    Update colecciones.py

--- Colecciones.py
+++ Colecciones.py
@@ -16,3 +16,11 @@ String > Floats > int > boolean (gerarquia ed mayor peso en memotia)
 tupla = (,) (una tupla es inmutable)
 tupla = tuple()
 """
+import numpy as np
+from numpy import array
+
+lista_mixta = ["Hola mundo", 1.01, 1, True, False]
+
+lista_mixta_numpy = np.array([1, 2, 3, 4, 5])
+
+
