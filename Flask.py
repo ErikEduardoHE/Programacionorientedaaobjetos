c2498a47c57dcfee2586d4e450a157b1006cc345
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-04-29 09:03:47 -0600

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+ALLOWED_IP = '10.2.81.75'
+
 @app.route('/')
 def hola_mundo():
     return 'Hola mundo'
 
 if __name__ == '__main__':
-    app.run(host='10.2.81.75', port=5000)
+    app.run(host='0.0.0.0', port=5000)
