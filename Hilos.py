28ddd5610d44fc75017c7f2d778ecc31ea6bafab
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-05-06 08:53:44 -0600

    Update Hilos.py

--- Hilos.py
+++ Hilos.py
@@ -14,8 +14,8 @@ class hilo(threading.Thread):
             sleep(self.intervalo)
         print(f"El hilo {self.nombre} ha finalizado")
 
-hilo1 = hilo("hilo", 2)
-hilo2 = hilo("hilo2", 4)
+hilo1 = hilo("hilo_1", 2)
+hilo2 = hilo("hilo_2", 4)
 hilo1.start()
 hilo2.start()
 hilo1.join()
