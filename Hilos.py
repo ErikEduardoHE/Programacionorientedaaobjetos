0e666aa4280cea55641999d91dd21063dcdcd557
Erik Eduardo <erik80622021@uap.uaz.edu.mx>
2025-05-06 08:45:22 -0600

    Upload Hilos.py

---
+++ Hilos.py
@@ -0,0 +1,15 @@
+import threading
+from time import sleep
+
+class hilo(threading.Thread):
+    def __init__(self, nombre, intervalo):
+        super().__init__()
+        self.nombre = nombre
+        self.intervalo = intervalo
+
+    def run(self):
+        print(f"El hilo {self.nombre} ha comenzado")
+        for i in range(5):
+            print(F"El hilo {self.nombre} se encuentra en iteracion {i}")
+            sleep(self.intervalo)
+        print(f"El hilo {self.nombre} ha finalizado")
